e02cfb924db61a84e251796906ebf65e0e129641
feger <marc.feger@uni-duesseldorf.de>
2025-11-18 20:07:30 +0100

    Add SCIARK

---
+++ datasets/SCIARK/sciark.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from pathlib import Path
+import json
+import os
+
+base_url = "https://raw.githubusercontent.com/TomatenMarc/GAIC-2026/refs/heads/main/datasets/FINARG"
+
+df_all = pd.read_csv('../../assets/all_data.csv')
+df_sample = pd.read_csv('../../assets/sample_42.csv')
+df_sample.rename(columns={"sentence": "processed_sentence"}, inplace=True)
+
+df_sciark = df_sample[df_sample["dataset"] == "SCIARK"]
+
+df_sciark = df_sciark.merge(df_all[["dataset_id", "sentence"]], on="dataset_id")
+
+df_sciark['dataset_id_clean'] = (
+    df_sciark['dataset_id']
+    .str.rsplit('_', n=1)
+    .str[0]
+)
+
+valid_ids = set(df_sciark['dataset_id_clean'])
+data_dir = Path(os.getcwd()+"/data/")  # <-- update if needed
+
+with open(os.getcwd() + "/data/SciARK.json", "r", encoding="utf-8") as f:
+    text_data = json.load(f)
+    text_data = {
+        key.replace(".txt", ""): value
+        for key, value in text_data.items()
+    }
+
+for id in valid_ids:
+    assert id in text_data.keys()
+    with open(os.getcwd() + "/data/" + id.replace("/", "_").replace(" ", "_") + ".txt", "w", encoding="utf-8") as out:
+        out.write("\n".join(text_data[id]["sentences"]))
+
+df_sciark["dataset_id"] = df_sciark["dataset_id"].apply(lambda row: row.replace("/", "_").replace(" ", "_"))
+df_sciark["dataset_id_clean"] = df_sciark["dataset_id_clean"].apply(lambda row: row.replace("/", "_").replace(" ", "_"))
+
+valid_ids = set(df_sciark['dataset_id_clean'])
+txt_files = set([file.stem for file in data_dir.glob("*.txt")])
+assert sorted(txt_files) == sorted(valid_ids)
+assert sorted(txt_files) == sorted(set((df_sciark['dataset_id'].str.rsplit('_', n=1).str[0])))
+
+for file in data_dir.glob("*.txt"):
+    if file.stem not in valid_ids:
+        os.remove(file)
+
+os.remove(os.getcwd() + "/data/SciARK.json")
+
+for _, row in df_sciark.iterrows():
+    file = open(os.getcwd() + "/data/" + row.dataset_id_clean + ".txt")
+    assert row.sentence in file.read(), f"{row.sentence} not in {row.dataset_id_clean}."
+
+df_sciark = df_sciark[["dataset", "dataset_id", "dataset_id_clean", "label", "sentence", "split"]]
+df_sciark.rename(columns={"dataset_id_clean": "document"}, inplace=True)
+df_sciark["document"] = df_sciark["document"].apply(lambda row: f"{base_url}/data/{row}.txt")
+df_sciark["guidelines"] = "-"
+df_sciark["paper"] = "https://aclanthology.org/2021.argmining-1.10/"
+
+df_sciark = df_sciark[["dataset", "dataset_id", "paper", "document", "guidelines", "split", "label", "sentence"]]
+for _, row in df_sciark.iterrows():
+    assert row.dataset_id.rsplit('_', 1)[0] in row.document
+df_sciark.to_csv(os.getcwd() + "/sciark.csv")
---
+++ datasets/SCIARK/sciark.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/afergadis/SciARK/refs/heads/main/dataset/SciARK.json
+mkdir -p ./data
+mv SciARK.json ./data

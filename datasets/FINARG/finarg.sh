791ee67c66df1c3755d7f8684bd5f0027f093dbc
feger <marc.feger@uni-duesseldorf.de>
2025-11-18 13:31:19 +0100

    Add LICENSE for FINARG

--- datasets/FINARG/finarg.sh
+++ datasets/FINARG/finarg.sh
@@ -1,4 +1,5 @@
 wget https://github.com/Alaa-Ah/The-FinArg-Dataset-Argument-Mining-in-Financial-Earnings-Calls/raw/refs/heads/main/FinArg.zip
+wget https://raw.githubusercontent.com/Alaa-Ah/The-FinArg-Dataset-Argument-Mining-in-Financial-Earnings-Calls/refs/heads/main/LICENSE
 mkdir -p data/
 
 unzip FinArg.zip -d ./data

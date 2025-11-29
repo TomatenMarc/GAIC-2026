cdeb9d091e85455d0380f4b418e25fcae599ecb2
feger <marc.feger@uni-duesseldorf.de>
2025-11-29 11:54:38 +0100

    Rename files for ARGUMINSCI

--- datasets/ARGUMINSCI/arguminsci.py
+++ datasets/ARGUMINSCI/arguminsci.py
@@ -38,6 +38,14 @@ df_arguminsci["dataset_id"] = df_arguminsci["dataset"] + "-" + df_arguminsci["da
 df_arguminsci.rename(columns={"dataset_id": "id"}, inplace=True)
 df_arguminsci = df_arguminsci[["id", "dataset_id_clean", "label", "sentence", "split"]]
 df_arguminsci.rename(columns={"dataset_id_clean": "document"}, inplace=True)
+
+unique_document_names = df_arguminsci["document"].unique()
+anonymized_document_names = [f"ARGUMINSCI-{i+1}" for i in range(len(unique_document_names))]
+
+for original, new in zip(unique_document_names, anonymized_document_names):
+    os.rename(os.getcwd() + "/data/" + original + ".txt", os.getcwd() + "/data/" + new + ".txt")
+
+df_arguminsci["document"] = df_arguminsci["document"].apply(lambda row: dict(zip(unique_document_names, anonymized_document_names))[row])
 df_arguminsci["document"] = df_arguminsci["document"].apply(lambda row: f"{base_url}/data/{row}.txt")
 df_arguminsci["guidelines"] = "-"
 df_arguminsci["paper"] = "https://www.aclweb.org/anthology/W18-5206/"
@@ -49,3 +57,7 @@ for split in ["train", "dev", "test"]:
     df_ = df_.reset_index(drop=True)
     df_["id"] = f"ARGUMINSCI-{split}-" + (df_.index + 1).astype(str)
     df_.to_json(os.getcwd() + f"/arguminsci_{split}.jsonl", orient="records", lines=True)
+
+for txt_file in data_dir.glob("*.txt"):
+    if txt_file.stem not in anonymized_document_names:
+        os.remove(txt_file)

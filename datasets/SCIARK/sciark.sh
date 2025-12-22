fc7354bb9d1344013aca5de9d4b2b36b215656d7
feger <marc.feger@uni-duesseldorf.de>
2025-12-22 17:21:28 +0100

    Update SCIARK

--- datasets/SCIARK/sciark.sh
+++ datasets/SCIARK/sciark.sh
@@ -1,3 +1,7 @@
 wget https://raw.githubusercontent.com/afergadis/SciARK/refs/heads/main/dataset/SciARK.json
 mkdir -p ./data
 mv SciARK.json ./data
+
+mkdir -p ./paper
+wget https://aclanthology.org/2021.argmining-1.10.pdf
+mv ./2021.argmining-1.10.pdf ./paper/SCIARK.pdf
